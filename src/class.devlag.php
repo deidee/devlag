a671100e8bfe009bca047bd5e262f1f00129d5db
acj <gitlab@acjs.net>
2019-12-02 23:35:06 +0100

    Parsing PoC.

--- src/class.devlag.php
+++ src/class.devlag.php
@@ -15,4 +15,20 @@ class Devlag
     {
         return constant(get_class($this) . '::' . $this->source);
     }
+
+    public function getContents()
+    {
+        $source = constant(get_class($this) . '::' . $this->source);
+
+        $contents = file_get_contents($source);
+
+        return $contents;
+    }
+
+    public function parse()
+    {
+        header('Content-Type: image/svg+xml');
+
+        echo $this->getContents();
+    }
 }
---
+++ test/parse.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once '../src/class.devlag.php';
+
+$devlag = new Devlag;
+$devlag->parse();
